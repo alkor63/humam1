4be18d49859f4b646ab8a1396ac2519d84f4c729
alkor63 <alkor@medi-spa.ru>
2022-12-12 18:01:29 +0300

    second step task 2

--- src/Human.java
+++ src/Human.java
@@ -2,10 +2,11 @@ public class Human {
     public String name;
     public String town;
     public int yearOfBirth;
+    public String jobTitle;
 
     @Override
     public String toString() {
         return "Привет! Меня зовут " + name + ". Я из города " + town + ". " +
-                "Я родился в " + yearOfBirth + " году. Будем знакомы!";
+                "Я родился в " + yearOfBirth + " году. Я работаю на должности " +jobTitle+ ". Будем знакомы!";
     }
 }
