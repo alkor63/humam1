5b76d506afe79931011ef2397a11a5be86bf757d
alkor63 <alkor@medi-spa.ru>
2022-12-15 18:21:11 +0300

    second step task 2-1

--- src/Human.java
+++ src/Human.java
@@ -5,13 +5,13 @@ public class Human {
     private String jobTitle;
 
     public Human(String name, String town, int yearOfBirth, String jobTitle) {
-        if(name == null || name.equals("")) name = "Информация не указана";
+        if(name == null || name.isEmpty()) name = "Информация не указана";
         this.name = name;
-        if(town == null || town.equals("")) town = "Информация не указана";
+        if(town == null || town.isEmpty()) town = "Информация не указана";
         this.town = town;
         if (yearOfBirth < 0) yearOfBirth = 0;
         this.yearOfBirth = yearOfBirth;
-        if(jobTitle == null || jobTitle.equals("")) jobTitle = "Информация не указана";
+        if(jobTitle == null || jobTitle.isEmpty()) jobTitle = "Информация не указана";
         this.jobTitle = jobTitle;
     }
 
@@ -19,5 +19,23 @@ public class Human {
     public String toString() {
         return "Привет! Меня зовут " + name + ". Я из города " + town + ". " +
                 "Я родился в " + yearOfBirth + " году. Я работаю на должности " +jobTitle+ ". Будем знакомы!";
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth < 0) yearOfBirth = 0;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        if(town == null || town.isEmpty()) town = "Информация не указана";
+        this.town = town;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        Human[] humans = new Human[4];
+        Human[] humans = new Human[5];
         humans[0] = new Human("","Минск", 1988, "бренд-менеджер");
-        System.out.println(humans[0]);
-
         humans[1] = new Human("Аня", "", 1993, "методист образовательных программ");
-        System.out.println(humans[1]);
-
         humans[2] = new Human("Катя", "Калининград", 1992, null);
-        System.out.println(humans[2]);
-
         humans[3] = new Human("Артем", "Москва", -1995,"директор по развитию бизнеса");
-        System.out.println(humans[3]);
+        humans[4] = new Human("Владимир", "Казань", 2001, "безработный");
 
+        for (Human hp:humans)
+        System.out.println(hp);
     }
 }
