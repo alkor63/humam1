730f1a3f18c15533b0854c4048401071b8ce7f0c
alkor63 <alkor@medi-spa.ru>
2022-12-19 09:47:38 +0300

    second step task 2-1

--- src/Bouquet.java
+++ src/Bouquet.java
@@ -1,68 +1,55 @@
-public class Bouquet extends Flower {
-    private int numFlower1;
-    private int numFlower2;
-    private int numFlower3;
-    private int numFlower4;
-    private int minLifeSpan;
-    private double bouquetCost;
-    Flower f1 = new Flower();
+import java.text.DecimalFormat;
 
-    public Bouquet(int numFlower1, Flower f1)    {
-        minLifeSpan = f1.getLifeSpan();
-        bouquetCost = numFlower1 * f1.getCost() * 1.1;
-        this.numFlower1 = numFlower1;
-        System.out.println("Состав букета: "+numFlower1+" "+f1.getFlowerName());
+public class Bouquet {
+    private int[] numFlower;
+    private final Flower[] flowers;
+
+    public static double costOfBouquet(int[] numFlower, Flower[] flowers) {
+        double bouquetCost = 0;
+        for (int j = 0; j < flowers.length; j++) {
+            bouquetCost += numFlower[j] * flowers[j].getCost();
+        }
+        return bouquetCost * 1.1;
     }
 
-    public Bouquet(int numFlower1, Flower f1, int numFlower2, Flower f2)    {
-        minLifeSpan = f1.getLifeSpan();
-        if (minLifeSpan > f2.getLifeSpan()) minLifeSpan = f2.getLifeSpan();
-        bouquetCost = (numFlower1 * f1.getCost() + numFlower2 * f2.getCost())*1.1;
-        this.numFlower1 = numFlower1;
-        this.numFlower2 = numFlower2;
-        System.out.println("Состав букета: \t"+f1.getFlowerName()+" "+numFlower1+" штук\n\t\t\t\t"
-                +f2.getFlowerName()+" "+numFlower2+" штук");
+    public static int minLifeSpan(int[] numFlower, Flower[] flowers) {
+        int minLife;
+        int j;
+        for (j = 0; j < numFlower.length; j++) {
+            if (numFlower[j] > 0) break;
+        }
+        minLife = flowers[j].getLifeSpan();
+        for (j++; j < flowers.length; j++) {
+            if (numFlower[j] > 0) {
+
+                if (minLife > flowers[j].getLifeSpan())
+                    minLife = flowers[j].getLifeSpan();
+            } else break;
+        }
+        return minLife;
     }
 
-    public Bouquet(int numFlower1, Flower f1, int numFlower2, Flower f2, int numFlower3, Flower f3)
-    {
-        minLifeSpan = f1.getLifeSpan();
-        if (minLifeSpan > f2.getLifeSpan()) minLifeSpan = f2.getLifeSpan();
-        if (minLifeSpan > f3.getLifeSpan()) minLifeSpan = f3.getLifeSpan();
-        bouquetCost = (numFlower1 * f1.getCost() + numFlower2 * f2.getCost() + numFlower3 * f3.getCost())*1.1;
-        this.numFlower1 = numFlower1;
-        this.numFlower2 = numFlower2;
-        this.numFlower3 = numFlower3;
-        System.out.println("Состав букета: \t"+f1.getFlowerName()+" "+numFlower1+" штук\n\t\t\t\t"
-            +f2.getFlowerName()+" "+numFlower2+" штук\n\t\t\t\t" +
-                f3.getFlowerName()+" "+numFlower3+" штук");
+    public Bouquet(int[] numFlower, Flower[] flower) {
+        this.flowers = flower;
+        this.numFlower = numFlower;
+        System.out.println("Состав букета: ");
+        for (int i = 0; i < numFlower.length; i++) {
+            if (numFlower[i] < 0) {
+                System.out.println("!!!!! в букете указано отрицательное количество цветов !!!!!!!!!");
+                System.out.println("\t\t\t" + flower[i].getFlowerName() + " - " + numFlower[i] + " щт.");
+                break;
+            }
+            if (numFlower[i] > 0) {
+                System.out.println("\t\t\t" + flower[i].getFlowerName() + " - " + numFlower[i] + " щт.");
+            }
+        }
     }
 
-    public Bouquet(int numFlower1, Flower f1, int numFlower2, Flower f2, int numFlower3, Flower f3, int numFlower4, Flower f4)
-    {
-        minLifeSpan = f1.getLifeSpan();
-        if (minLifeSpan > f2.getLifeSpan()) minLifeSpan = f2.getLifeSpan();
-        if (minLifeSpan > f3.getLifeSpan()) minLifeSpan = f3.getLifeSpan();
-        if (minLifeSpan > f4.getLifeSpan()) minLifeSpan = f4.getLifeSpan();
-        bouquetCost = 1.1*(numFlower1 * f1.getCost() + numFlower2 * f2.getCost() + numFlower3 * f3.getCost() + numFlower4 * f4.getCost());
-        this.numFlower1 = numFlower1;
-        this.numFlower2 = numFlower2;
-        this.numFlower3 = numFlower3;
-        this.numFlower4 = numFlower4;
-        System.out.println("Состав букета: \t"+f1.getFlowerName()+" "+numFlower1+" штук\n\t\t\t\t"
-                +f2.getFlowerName()+" "+numFlower2+" штук\n\t\t\t\t" +
-                f3.getFlowerName()+" "+numFlower3+" штук\n\t\t\t\t" +
-                f4.getFlowerName()+" "+numFlower4+" штук");
-    }
+    DecimalFormat dF = new DecimalFormat("#.00");
 
     @Override
     public String toString() {
-        return "финальная стоимость букета для покупателя " + dF.format(bouquetCost) +" рублей. Срок стояния букета "
-                + minLifeSpan + " дней";
+        return "финальная стоимость букета для покупателя " + dF.format(costOfBouquet(numFlower, flowers)) + " рублей.\n\t Срок стояния букета "
+                + minLifeSpan(numFlower, flowers) + " дней";
     }
-//
-//    private void printCost(double costOfBouquet, int minLifeSpan) {
-//                System.out.println("финальная стоимость букета для покупателя " + costOfBouquet +
-//                "рублей. Срок стояния букета " + minLifeSpan + " дней");
-//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,28 @@ public class Main {
     public static void main(String[] args) {
 
         Human[] humans = new Human[5];
-        humans[0] = new Human("","Минск", 1988, "бренд-менеджер");
+        humans[0] = new Human("", "Минск", 1988, "бренд-менеджер");
         humans[1] = new Human("Аня", "", 1993, "методист образовательных программ");
         humans[2] = new Human("Катя", "Калининград", 1992, null);
-        humans[3] = new Human("Артем", "Москва", -1995,"директор по развитию бизнеса");
+        humans[3] = new Human("Артем", "Москва", -1995, "директор по развитию бизнеса");
         humans[4] = new Human("Владимир", "Казань", 2001, "безработный");
 
-        for (Human hp:humans)
-        System.out.println(hp);
+        for (Human human : humans) {
+            System.out.println(human);
+        }
 
-        Flower rose = new Flower("роза","Голландия",35.598520,0);
-        Flower chrysanthemum = new Flower("хризантема",null,15,5);
-        Flower peony = new Flower("пион","Англия",69.9,1);
-        Flower gypsophila = new Flower("гипсофила","Турция",19.5,10);
+        Flower rose = new Flower("роза", "Голландия", 35.598520, 0);
+        Flower chrysanthemum = new Flower("хризантема", null, 15, 5);
+        Flower peony = new Flower("пион", "Англия", 69.9, 1);
+        Flower gypsophila = new Flower("гипсофила", "Турция", 19.5, 10);
+        Flower[] flowers = new Flower[]{rose, chrysanthemum, peony, gypsophila};
+        int[] numFlowers = new int[]{3, 5, 0, 1};
 
-        System.out.println(rose);
-        System.out.println(chrysanthemum);
-        System.out.println(peony);
-        System.out.println(gypsophila);
-        Bouquet bouquet = new Bouquet(5,rose, 7,peony);
+        for (Flower flower : flowers) {
+            System.out.println(flower);
+        }
+
+        Bouquet bouquet = new Bouquet(numFlowers, flowers);
         System.out.println(bouquet);
-        Bouquet bouquet2 = new Bouquet(5,rose, 7,peony,3,chrysanthemum,2,gypsophila);
-        System.out.println(bouquet2);
     }
 }
